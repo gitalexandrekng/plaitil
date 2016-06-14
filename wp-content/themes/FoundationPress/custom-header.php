9ef2ad01f584f34bd5ca91771615a3cba556e0fe
alexandrekong <alexandre.kong@gmail.com>
2016-06-14 03:16:45 +0200

    d

--- wp-content/themes/FoundationPress/custom-header.php
+++ wp-content/themes/FoundationPress/custom-header.php
@@ -20,6 +20,12 @@
     <!-- DEBUT DU SLIDE
     ====================== -->
 
+    <?php $args = array( 'post_type' => 'portfolio', 'posts_per_page' => 10 ); ?>
+    <?php $loop = new WP_Query( $args ); ?>
+    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+    <?php endwhile; ?>
+
     <div class="wrapper">
         <div class="row small-collapse fullwidth">
             <div class="small-3 columns" style="height:100%;">
@@ -29,16 +35,42 @@
             </div>
             <div class="small-9 columns" style="height:100%">
                 <div class="right">
-                    <div class="sliderz"></div>
-                    <div class="titlez">
-                        <div class="describe">
-                            <div class="barre"></div>
-                            <div class="texte">Super portfolio de swag website</div>
-                        </div>
-                        <div class="intitlez">
-                            SWAG<br>website
-                        </div>
+
+                    <!-- Section 1 -->
+
+                    <div id="owlslide" class="owl-carousel">
+                        <?php $args = array( 'post_type' => 'portfolio', 'posts_per_page' => 2 ); ?>
+                        <?php $loop = new WP_Query( $args ); ?>
+                        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                            <div class="item" data-hash="<?php the_ID() ?>">
+                                <div class="sliderz" style="background-image:url(<?php the_post_thumbnail_url() ?>)"></div>
+                                <div class="fade"></div>
+                            </div>
+                        <?php endwhile; ?>
                     </div>
+
+                    <!-- Section 2 -->
+                    <div id="owltitle" class="owl-carousel">
+                        <?php $args = array( 'post_type' => 'portfolio', 'posts_per_page' => 2 ); ?>
+                        <?php $loop = new WP_Query( $args ); ?>
+                        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                            <div class="item titlez" data-hash="<?php the_ID() ?>">
+                                <div class="describe">
+                                    <div class="barre"></div>
+                                    <div class="texte">Super portfolio de swag website</div>
+                                </div>
+                                <div class="intitlez"><?php echo the_title() ?></div>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
+
+                    <!-- Section 3 -->
+                    <div class="type">
+                        <div class="intype">Art direction / UI / UX DEISGN</div>
+                    </div>
+
+                    <!-- section 4 -->
+                    <div class="carrerouge"></div>
                 </div>
             </div>
         </div>
@@ -55,20 +87,27 @@
                             <div class="incontact">Contact</div>
                         </a>
                         <div class="row small-collapse fullwidth" style="height:50%;position:relative;z-index:5;">
-                            <a class="small-6 columns inz" href="#">
-                                <div class="fond" style="background-image:url(<?php echo get_stylesheet_directory_uri() ?>/images/src/swag-3.png)">
-                                </div>
-                            </a>
-                            <a class="small-6 columns inz" href="#">
-                                <div class="fond" style="background-image:url(<?php echo get_stylesheet_directory_uri() ?>/images/src/swag-2.png)">
-                                </div>
-                            </a>
+                            <?php $args = array( 'post_type' => 'portfolio', 'posts_per_page' => 2 ); ?>
+                            <?php $loop = new WP_Query( $args ); ?>
+                            <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                                <a class="small-6 columns inz" href="#<?php the_ID() ?>">
+                                    <div class="fond" style="background-image:url(<?php the_post_thumbnail_url() ?>)">
+                                    </div>
+                                </a>
+                            <?php endwhile; ?>
                         </div>
                         <div class="carre"></div>
                     </div>
 
                     <!-- red cube -->
                     <div class="inleft"></div>
+
+                    <!-- more -->
+                    <div class="more">
+                        <div class="in">
+                            <div class="texte">Voir plus</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="small-6 columns" style="height:100%;"></div>
--- wp-content/themes/FoundationPress/customjs.js
+++ wp-content/themes/FoundationPress/customjs.js
@@ -9,3 +9,28 @@ $('.inz').hover(function(){
         transform:'scale(1)'
     })
 })
+
+
+$('#owlslide').owlCarousel({
+    items:1,
+    URLhashListener:true,
+    autoplayHoverPause:true,
+    loop:true,
+    startPosition: 'URLHash',
+    mouseDrag: false,
+    animateOut: 'slideOutDown',
+    smartSpeed:450
+});
+
+$('#owltitle').owlCarousel({
+    items:1,
+    URLhashListener:true,
+    autoplayHoverPause:true,
+    loop:true,
+    startPosition: 'URLHash',
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    mouseDrag: false,
+});
+
+$('#owlslide .item').css('height', $('#owlslide'))
